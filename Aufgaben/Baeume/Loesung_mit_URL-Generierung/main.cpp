9350c05a907b2ac38c540dcc62962f5ba022be4b
ReinerHuechting <46283923+ReinerHuechting@users.noreply.github.com>
2021-06-11 14:45:46 +0200

    Variante mit generierted GraphViz-URL hinzugefügt.

---
+++ Aufgaben/Baeume/Loesung_mit_URL-Generierung/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "tree.h"
+using namespace std;
+
+void basicTreeTests();
+
+int main() {
+  basicTreeTests();
+  
+  return 0;
+}
+
+void basicTreeTests() {
+  Tree t1;
+  
+  t1.insert("Haus", "Da wohnt man.");
+  t1.insert("Garten", "Da auch!");
+  t1.insert("Garage", "Hier steht mein Auto.");
+  t1.insert("Schuppen", "... und hier mein Fahrrad.");
+  t1.insert("Tür", "Irgendwie muss ich ja reinkommen.");
+  
+  cout << t1.inOrderValues() << endl << endl;
+  cout << t1.pathToElement("Schuppen") << endl;
+  cout << t1.dotString() << endl << endl;
+  cout << t1.dotURL() << endl;
+}
